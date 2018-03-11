ca5516cc8b6e1ba59e97cc0b33ab39ebc2d832d5
Ninad Sinha <sinha104@umn.edu>
2018-03-11 17:45:57 -0500

    A lot of files were modified. Also, stuff for transistions was taken from alvaro trigo.Have to add in links for social networks and education and experience.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 //insert global variables in here
 var lastYscrollPos = window.pageYOffset;
 
-
-
-
-
 //functions start here
 
-window.onscroll = function(){scrollfunction()}
+//window.onscroll = function(){scrollfunction()}
 
 function scrollfunction(){
+	alert("last windows position was "+lastYscrollPos+" scrollY was "+scrollY);
+	alert("Value of check was "+(lastYscrollPos>scrollY));
 	if(lastYscrollPos > scrollY){
-		window.scrollto(0,this.lastYscrollPos + window.innerHeight);
-		this.lastYscrollPos += window.innerHeight;
-	}else{
-		window.scrollto(0,this.lastYscrollPos - window.innerHeight);
+		window.scrollTo(0,this.lastYscrollPos - window.innerHeight);
 		this.lastYscrollPos -= window.innerHeight;
+	}else{
+		window.scrollTo(0,this.lastYscrollPos + window.innerHeight);
+		this.lastYscrollPos += window.innerHeight;
 	}
 }
